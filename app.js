34056cc83ec437eae59dce89a45c45a48092f5b8
senquevila <senquevila@gmail.com>
2014-03-24 01:59:39 -0600

    Agregr

--- app.js
+++ app.js
@@ -1,41 +1,82 @@
 var express = require('express')
   , download = require('download.js')
   , cheerio = require('cheerio')
+  , buff// = require('buffer')
 	, app = express()
 	, url = "http://www.empleos.hn/index.php?cat=90"
-	, element = "a.job_list_title";
+  , localhost = "http://www.empleos.hn/"
+	, element = "table#joblist tr";
 
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
 
+buff = '<h1>Ofertas de trabajo</h1>';
+
+for (var i = 0; i < 5; i++) {
+  var offset = i * 10;
+  var p_url = url + '&offset=' + offset;
+
+  download(p_url, function(data) {
+    if (data) {
+      var $ = cheerio.load(data.toString('utf-8'));
+      buff = '<table>';
+
+      $(element).each(function(i, e) {
+        buff += '<tr>';
+
+        //if (i==1){
+          var child = $(e.children)[i];
+          console.log(i + '::' + $(child).text());
+          console.log('..................');
+        //}
+
+        $(e.children).each(function(j, e1){
+          var childrenStr = $(e1.children).text().trim();
+          /*var bShow = false;
+          var diff = 0;
+          var find = 0;
+
+          if (i == 0)
+            bShow = true;
+          else if (i > 0 && j == 1) {
+            var hoy = new Date();
+            var fecha = new Date(childrenStr);
+            diff = (hoy - fecha) / 1000 / 3600 / 24;
+            if (diff > 10) {
+              bShow = false;
+            } else {
+              bShow = true;
+              find = i;
+            }
+          }
+
+          if (i == find)
+            bShow = true;
+
+          if (bShow)
+            buff += '<td>' + childrenStr + '</td>';
+          */
+        });
+
+        buff += '</tr>';
+      });
+        
+      buff += '</table';
+    }
+    else 
+      console.error("error");  
+  });
+}
 
 app.set('title', 'Bolsa de trabajo');
 app.get('title');
 
 app.get('/', function(req, res){
-	res.send('<h1>Ofertas de trabajo recientes</h1>');
 
-	for (var i = 0; i < 10; i++) {
-		var offset = i * 10;
-		var p_url = url + '&offset=' + offset;
-
-    download(p_url, function(data) {
-      if (data) {
-        //console.log(data);
-
-        var $ = cheerio.load(data);
-
-        $(element).each(function(i, e) {
-            var chunk = $(e).text();
-
-            console.log(chunk);
-            res.set('Content-Type', 'text/html');
-            res.send(chunk);
-          });
-          
-        console.log("done");
-      }
-      else console.log("error");  
-    });
-	}
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(buff.toString('utf-8'));
 });
 
 app.listen(3000);
